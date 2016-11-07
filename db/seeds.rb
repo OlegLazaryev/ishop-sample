6dff85298e9a8a28f6d1b05a9c16f521b97f8ef8
OlegL <olegl@softwareplanetgroup.com>
2016-11-08 00:43:38 +0200

    Add products

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+%w(	
+	Aunt Hattie's
+    Back to Nature (Mondelēz International)
+    Danish
+    Cobblestone
+    Dave's Killer Bread
+    Entenmann's
+    Famous Amos
+    Home Pride
+    Hovis[2]
+    Keebler Company
+    Kits
+    McVitie's
+    Mother's Pride
+    Nabisco[3]
+    Nature's Own
+    Newman's Own
+    Pepperidge Farm
+    Roman Meal
+    St. Amour Cookies
+    Sunbeam Bread
+    tastyKakes
+    Warburtons
+    Westminster Cracker Company[4]
+    Wonder Bread
+).each do |product_name|
+	Product.create!(name: product_name, price: rand(2.2..1050), description: BetterLorem.w(rand(0..40)))
+end
